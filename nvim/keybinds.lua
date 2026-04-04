defe37e5125960081ffa247db508e19926a18a77
matero952 <mateo@mateodev.zephyrus>
2026-04-04 00:13:26 -0600

    added more keybinds and functionality

--- nvim/keybinds.lua
+++ nvim/keybinds.lua
@@ -1,11 +1,25 @@
 function disable_arrow_keys()
 	local key_names = {"<Up>", "<Down>", "<Left>", "<Right>"}
-	local modes = {"n", "i", "v", "c", "t"}
+	local modes = {"n", "v", "c", "t"}
 	for i, key in ipairs(key_names) do
 		for j, mode in ipairs(modes) do
 			vim.keymap.set(mode, key, "<Nop>")
 		end
 	end
+end
+
+function open_file_in_buffer()
+  local user_input = vim.fn.input("file name:")
+  if user_input ~= "" then
+    local cmd = "e " .. user_input
+    vim.cmd(cmd)
+  end
+end
+
+function save_no_quit()
+  local cmd = "wa"
+  vim.cmd(cmd)
+  vim.print("saved!")
 end
 
 disable_arrow_keys()
@@ -15,6 +29,7 @@ vim.keymap.set("n", "<Leader>r", [[:%s/\<<C-r><C-w>\>//g<Left><Left>]])
 
 vim.keymap.set("n", "<Leader>vl", ":vsplit<CR>")
 vim.keymap.set("n", "<Leader>hl", ":split<CR>")
+vim.keymap.set("n", "<Leader>bo", open_file_in_buffer)
 
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
@@ -28,4 +43,4 @@ vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-
+vim.keymap.set("n", "WW", save_no_quit)
--- nvim/ui.lua
+++ nvim/ui.lua
@@ -1,4 +1,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
-
+vim.go.statusline = "FILE %t MODIFIED %m %= FT %Y LOC %l:%v"
+vim.go.winbar = "Buffer #: %{bufnr('%')}"
