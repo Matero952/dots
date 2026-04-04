514a2f8d9f31c6748d67cd6212162e64fc815565
matero952 <mateo@mateodev.zephyrus>
2026-04-03 23:45:34 -0600

    added useful buffer keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,3 @@
 package.path = package.path .. ";/home/mateo/.config/nvim/?.lua"
-require("bindings")
+require("keybinds")
 require("ui")
---
+++ nvim/keybinds.lua
@@ -0,0 +1,31 @@
+function disable_arrow_keys()
+	local key_names = {"<Up>", "<Down>", "<Left>", "<Right>"}
+	local modes = {"n", "i", "v", "c", "t"}
+	for i, key in ipairs(key_names) do
+		for j, mode in ipairs(modes) do
+			vim.keymap.set(mode, key, "<Nop>")
+		end
+	end
+end
+
+disable_arrow_keys()
+
+vim.g.mapleader =" "
+vim.keymap.set("n", "<Leader>r", [[:%s/\<<C-r><C-w>\>//g<Left><Left>]])
+
+vim.keymap.set("n", "<Leader>vl", ":vsplit<CR>")
+vim.keymap.set("n", "<Leader>hl", ":split<CR>")
+
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<CR>")
+vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<CR>")
+vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<CR>")
+vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+
